d20bdcc10ba2327b2468af5a090b8bb8b82460eb
kumariBhavya <bhavya.kumari208@gmail.com>
2023-05-23 12:03:41 +0530

    this is mlm project

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\HomeContoller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[HomeContoller::class,'index'])->name('user.home');
